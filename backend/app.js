23d8ac5df40c287c74f93af3f2fae57b02368609
Lokesh Panchal <113035488+cypher2509@users.noreply.github.com>
2024-11-09 17:39:28 -0330

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//all apis should respond in json.
+//all http requests should respond in JSON objects and should follow a proper camel case convention.
+// all http requests will get data in form of JSON objects.
 
+// Use routes for http requests.
+
+//all the routes should be named after entity.
+// eg. get.("/order", function())
+// eg. post.("/order", function())
+// eg. put.("/order", function())
+// eg. delete.("/order", function())
+
+// add comments for all the http requests giving a brief description about the task it performs.
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
